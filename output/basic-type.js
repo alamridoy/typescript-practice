d34e703c299811d9b9c79359e0211c89a79b7d2a
alam ridoy <alamridoy7@gmail.com>
2024-03-04 23:51:41 +0600

    explicit and union

---
+++ output/basic-type.js
@@ -0,0 +1,14 @@
+"use strict";
+// variable
+let a;
+let b;
+a = "aaaa";
+b = 34;
+console.log(a);
+console.log(b);
+// array
+let arr = [];
+arr = ['ee', 'ee', 1];
+// object
+let obj;
+obj = [];
---
+++ src/basic-type.ts
@@ -0,0 +1,27 @@
+// variable
+let a: string;
+let b: number;
+
+
+a = "aaaa"
+
+b = 34
+
+console.log(a)
+console.log(b)
+
+
+
+
+// array
+let arr: (string | number)[] = []
+
+arr = ['ee', 'ee', 1]
+
+
+
+// object
+
+let obj: object;
+
+obj = []
